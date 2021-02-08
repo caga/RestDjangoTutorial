a7cdd4ea020948cd8afa4dbc1dda32199e3817b5
cagatay <osman@kayi.oba>
2021-02-08 06:05:19 +0000

    tutorial

---
+++ tutorial/tutorial/urls.py
@@ -0,0 +1,16 @@
+from django.urls import include, path
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register (r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+ 
+ # Wire up our UPI using atuomatic URL routing
+ # Additionall, we inclid login URLs for teh browsable UPI.
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path ('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls')),
+ ]
